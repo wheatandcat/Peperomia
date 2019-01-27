880c0bbdbed683b7b9e1f4ba25e5d638d371834d
wheatandcat <wheatandcat@gmail.com>
2019-01-27 23:53:44 +0900

    add cloud function

---
+++ cloudFunction/save_item.go
@@ -0,0 +1,53 @@
+package app
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+type Item struct {
+	Status string `json:"status"`
+}
+
+// HelloGet is an HTTP Cloud Function.
+func SaveItem(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+		"first": "Ada",
+		"last":  "Lovelace",
+		"born":  1815,
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	i := Item{Status: "OK"}
+	res, err := json.Marshal(i)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(res)
+}
+
+// [END functions_helloworld_get]
