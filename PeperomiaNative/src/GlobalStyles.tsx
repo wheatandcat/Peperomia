2f60d705478f05c8b625fbcfda91dbcade64188f
wheatandcat <wheatandcat@gmail.com>
2019-05-17 23:46:44 +0900

    update SafeAreaView close #5

--- PeperomiaNative/src/GlobalStyles.tsx
+++ PeperomiaNative/src/GlobalStyles.tsx
@@ -1,8 +1,6 @@
 import { StyleSheet, Platform } from "react-native";
 export default StyleSheet.create({
   droidSafeArea: {
-    flex: 1,
-    backgroundColor: "#fff",
     paddingTop: Platform.OS === "android" ? 30 : 0
   }
 });
