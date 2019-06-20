f655394b0886a60c457b82f0539aec37dbc71884
Yohei Iino <wheatandcat@gmail.com>
2019-06-20 21:21:33 +0900

    fxi build close #22 (#23)
    * fxi build close #22
    
    * typo

---
+++ PeperomiaNative/setupDevelopment.sh
@@ -0,0 +1,4 @@
+cp -r app.development.json app.json
+cp -r .env.development .env
+cd android
+cp -r google-services.development.json google-services.json
---
+++ PeperomiaNative/setupRelease.sh
@@ -0,0 +1,4 @@
+cp -r app.product.json app.json
+cp -r .env.product .env
+cd android
+cp -r google-services.product.json google-services.json
