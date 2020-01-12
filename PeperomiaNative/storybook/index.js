da49f1e0c6cffd37f663a7ba9a3b2603a8daf83a
Yohei Iino <wheatandcat@gmail.com>
2020-01-12 21:22:53 +0900

    Update package (#348)
    * update: package
    
    * update: storybook v5.3
    
    * update: storybook v5.3

--- PeperomiaNative/storybook/index.js
+++ PeperomiaNative/storybook/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import React from 'react';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import {
   getStorybookUI,
   configure,
-  addDecorator
-} from "@storybook/react-native";
-import { AppearanceProvider } from "react-native-appearance";
-import ThemeProvider from "../src/containers/Theme.tsx";
-import { loadStories } from "./storyLoader";
-import "./rn-addons";
+  addDecorator,
+} from '@storybook/react-native';
+import { AppearanceProvider } from 'react-native-appearance';
+import ThemeProvider from '../src/containers/Theme.tsx';
+import { loadStories } from './storyLoader';
+import './rn-addons';
 
 export const provider = story => (
   <AppearanceProvider>
@@ -26,6 +26,10 @@ configure(() => {
 
 // Refer to https://github.com/storybooks/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({
+  port: 7007,
+  onDeviceUI: false,
+  host: 'localhost',
+});
 
 export default StorybookUIRoot;
