b51c767b98cbdc7ee132af61d028f79c1c4eb511
wheatandcat <wheatandcat@gmail.com>
2020-02-29 18:18:39 +0900

    add: workflows/danger.yml

--- PeperomiaNative/dangerfile.ts
+++ PeperomiaNative/dangerfile.ts
@@ -1,3 +1,21 @@
-import { message } from 'danger';
+import { schedule, message } from 'danger';
+import { istanbulCoverage } from 'danger-plugin-istanbul-coverage';
 
 message('hello danger');
+
+schedule(
+  istanbulCoverage({
+    reportFileSet: 'createdOrModified',
+
+    coveragePath: { path: './coverage/lcov.info', type: 'lcov' },
+
+    reportMode: 'warn',
+
+    threshold: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  })
+);
