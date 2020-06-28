9a923c18c15b84c8af5164c946d6760259f3701f
wheatandcat <wheatandcat@gmail.com>
2020-06-28 22:56:32 +0900

    expo version up 38

--- PeperomiaNative/App.tsx
+++ PeperomiaNative/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { AppRegistry } from 'react-native';
 import App from './src';
+import { STORY_BOOK_ENABLED } from 'react-native-dotenv';
 import StoryBookUI from './storybook';
 
-const AppContainer = () => <App />;
+AppRegistry.registerComponent('Peperomia', () => StoryBookUI);
 
-export default AppContainer;
+export default StoryBookUI;
 
 console.disableYellowBox = true;
 console.ignoredYellowBox = [
