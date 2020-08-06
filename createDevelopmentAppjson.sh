a8d4fdef9f9390562e45739fe85f8c693bf69b5b
wheatandcat <wheatandcat@gmail.com>
2020-08-07 00:42:22 +0900

    tmp

--- createDevelopmentAppjson.sh
+++ createDevelopmentAppjson.sh
@@ -2,6 +2,7 @@ iosGoogleSignInClientId=`jq  '.iosGoogleSignInClientId' config/development.json`
 sentryAuthToken=`jq '.sentryAuthToken' config/development.json`
 firebaseApiKey=`jq  '.firebaseApiKey' config/development.json`
 measurementId=`jq '.measurementId' config/development.json`
+bugsnagApiKey=`jq '.bugsnagApiKey' config/development.json`
 
 # ダブルコーテーションを除外
 iosGoogleSignInClientId=${iosGoogleSignInClientId:1}
@@ -16,4 +17,7 @@ firebaseApiKey=${firebaseApiKey%\"}
 measurementId=${measurementId:1}
 measurementId=${measurementId%\"}
 
-jq -n --arg iosGoogleSignInClientId $iosGoogleSignInClientId --arg sentryAuthToken $sentryAuthToken --arg firebaseApiKey $firebaseApiKey --arg measurementId $measurementId -f appBase.jq | tee app.json
+bugsnagApiKey=${bugsnagApiKey:1}
+bugsnagApiKey=${bugsnagApiKey%\"}
+
+jq -n --arg iosGoogleSignInClientId $iosGoogleSignInClientId --arg sentryAuthToken $sentryAuthToken --arg firebaseApiKey $firebaseApiKey --arg measurementId $measurementId --arg bugsnagApiKey $bugsnagApiKey -f appBase.jq | tee app.json
--- createProdutAppjson.sh
+++ createProdutAppjson.sh
@@ -2,7 +2,7 @@ iosGoogleSignInClientId=`jq  '.iosGoogleSignInClientId' config/product.json`
 sentryAuthToken=`jq '.sentryAuthToken' config/product.json`
 firebaseApiKey=`jq  '.firebaseApiKey' config/product.json`
 measurementId=`jq '.measurementId' config/product.json`
-
+bugsnagApiKey=`jq '.bugsnagApiKey' config/product.json`
 
 # ダブルコーテーションを除外
 iosGoogleSignInClientId=${iosGoogleSignInClientId:1}
@@ -17,4 +17,7 @@ firebaseApiKey=${firebaseApiKey%\"}
 measurementId=${measurementId:1}
 measurementId=${measurementId%\"}
 
-jq -n --arg iosGoogleSignInClientId $iosGoogleSignInClientId --arg sentryAuthToken $sentryAuthToken --arg firebaseApiKey $firebaseApiKey --arg measurementId $measurementId -f appBase.jq | tee app.json
+bugsnagApiKey=${bugsnagApiKey:1}
+bugsnagApiKey=${bugsnagApiKey%\"}
+
+jq -n --arg iosGoogleSignInClientId $iosGoogleSignInClientId --arg sentryAuthToken $sentryAuthToken --arg firebaseApiKey $firebaseApiKey --arg measurementId $measurementId --arg bugsnagApiKey $bugsnagApiKey -f appBase.jq | tee app.json
